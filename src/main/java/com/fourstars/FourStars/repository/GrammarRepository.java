e024bddfaee8bafb700f878626ecf05ea3221ad4
Dinh Duong <jenny.180820@gmail.com>
2025-06-20 20:41:26 +0700

    feat(database): join fetch & entitygraph for query
    join fetch & entitygraph for query database
    
    Closes: #21

--- src/main/java/com/fourstars/FourStars/repository/GrammarRepository.java
+++ src/main/java/com/fourstars/FourStars/repository/GrammarRepository.java
@@ -1,5 +1,9 @@
 package com.fourstars.FourStars.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -8,6 +12,10 @@ import com.fourstars.FourStars.domain.Grammar;
 
 @Repository
 public interface GrammarRepository extends JpaRepository<Grammar, Long>, JpaSpecificationExecutor<Grammar> {
+    @Override
+    @EntityGraph(attributePaths = { "category" })
+    Page<Grammar> findAll(Specification<Grammar> spec, Pageable pageable);
+
     boolean existsByCategoryId(Long categoryId);
 
     boolean existsByNameAndCategoryId(String name, Long categoryId);
