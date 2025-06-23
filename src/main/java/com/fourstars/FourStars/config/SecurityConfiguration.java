40a363a08a6d387648ddede3cef4e7c3d31b1d82
Dinh Duong <jenny.180820@gmail.com>
2025-06-24 01:11:18 +0700

    docs(swagger): config swagger
    Closes: #45

--- src/main/java/com/fourstars/FourStars/config/SecurityConfiguration.java
+++ src/main/java/com/fourstars/FourStars/config/SecurityConfiguration.java
@@ -67,9 +67,9 @@ public class SecurityConfiguration {
                 "/api/v1/auth/login",
                 "/api/v1/auth/register",
                 "/api/v1/auth/refresh",
-                "/uploads/**"
-                // Thêm các endpoint public khác nếu có (ví dụ: Swagger UI, health check)
-                // "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
+                "/uploads/**",
+                "/ws/**",
+                "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
         };
 
         http
