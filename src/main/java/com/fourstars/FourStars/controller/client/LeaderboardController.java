5ba00adf043696ff6c1faea0895f86a3177ec54e
Dinh Duong <jenny.180820@gmail.com>
2025-06-25 13:26:50 +0700

    docs(account): add docs for dashboard &  leaderboard
    Closes: #65

--- src/main/java/com/fourstars/FourStars/controller/client/LeaderboardController.java
+++ src/main/java/com/fourstars/FourStars/controller/client/LeaderboardController.java
@@ -4,6 +4,12 @@ import com.fourstars.FourStars.domain.response.ResultPaginationDTO;
 import com.fourstars.FourStars.domain.response.user.UserResponseDTO;
 import com.fourstars.FourStars.service.UserService;
 import com.fourstars.FourStars.util.annotation.ApiMessage;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/leaderboard")
+@Tag(name = "User Engagement API", description = "APIs for user statistics, progress, and leaderboards")
 public class LeaderboardController {
 
     private final UserService userService;
@@ -20,6 +27,10 @@ public class LeaderboardController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Get the user leaderboard", description = "Publicly available endpoint to retrieve a paginated list of top users, sorted by points.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the leaderboard")
+    })
     @GetMapping
     @ApiMessage("Fetch the user leaderboard, sorted by points")
     public ResponseEntity<ResultPaginationDTO<UserResponseDTO>> getLeaderboard(Pageable pageable) {
